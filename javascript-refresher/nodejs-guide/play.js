c2f8dfb9fc7dc44cb460ece6889d927d9890e354
Thomas Martinez <tmartinez114@gmail.com>
2020-11-14 13:38:25 -0500

    Arrow Functions

--- javascript-refresher/nodejs-guide/play.js
+++ javascript-refresher/nodejs-guide/play.js
@@ -4,10 +4,21 @@ const hasHobbies = true;
 
 age = 30;
 
-function summarieUser(userName, userAge, userHasHobby) {
-    return 'Name is ' + userName +
+const summarizeUser = (userName, userAge, userHasHobby) => {
+    return ( 'Name is ' + userName +
     ', age is ' + userAge +
-    ' and the user has hobbies: ' + userHasHobby;
+    ' and the user has hobbies: ' + userHasHobby
+    );
 }
+
+const add = (a, b) => a + b;
+
+const addOne = a => a + 1;
+
+const addRandom = () => 1 + 2;
+
+console.log(addOne(1));
+
+console.log(addRandom());
 
 console.log(summarieUser(name, age, hasHobbies));
