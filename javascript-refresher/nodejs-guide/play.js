7d75dd6bf8c5878c16c9d068436d813898931e09
Thomas Martinez <tmartinez114@gmail.com>
2020-11-14 14:06:29 -0500

    Object

--- javascript-refresher/nodejs-guide/play.js
+++ javascript-refresher/nodejs-guide/play.js
@@ -2,23 +2,33 @@ const name = 'Max';
 let age = 29;
 const hasHobbies = true;
 
-age = 30;
+// age = 30;
 
-const summarizeUser = (userName, userAge, userHasHobby) => {
-    return ( 'Name is ' + userName +
-    ', age is ' + userAge +
-    ' and the user has hobbies: ' + userHasHobby
-    );
-}
+// const summarizeUser = (userName, userAge, userHasHobby) => {
+//     return ( 'Name is ' + userName +
+//     ', age is ' + userAge +
+//     ' and the user has hobbies: ' + userHasHobby
+//     );
+// }
 
-const add = (a, b) => a + b;
+// const add = (a, b) => a + b;
 
-const addOne = a => a + 1;
+// const addOne = a => a + 1;
 
-const addRandom = () => 1 + 2;
+// const addRandom = () => 1 + 2;
 
-console.log(addOne(1));
+// console.log(addOne(1));
 
-console.log(addRandom());
+// console.log(addRandom());
 
-console.log(summarieUser(name, age, hasHobbies));
+// console.log(summarieUser(name, age, hasHobbies));
+
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+person.greet();
